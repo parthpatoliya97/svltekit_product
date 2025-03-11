b154fa6b2767cf2175b1f49f3f1388fef6cc5398
parth patoliya <parthpatoliya001@gmail.com>
2025-03-11 13:49:50 +0530

    add

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,25 +1,14 @@
-// import fs from 'fs';
-// import { APP_NAME } from '$env/static/private';
-
-// export async function load() {
-//     const data = JSON.parse(fs.readFileSync('content/header-footer.json', 'utf-8'));
-
-//     //set current year
-//     data['year'] = new Date().getFullYear();
-//     data['appName'] = APP_NAME
-
-//     return data;
-// };
-
 import fs from 'fs';
-import { PUBLIC_APP_NAME } from '$env/static/public';
+import { APP_NAME } from '$env/static/private';
 
 export async function load() {
     const data = JSON.parse(fs.readFileSync('content/header-footer.json', 'utf-8'));
 
-    
+    //set current year
     data['year'] = new Date().getFullYear();
-    data['appName'] = PUBLIC_APP_NAME; // Use updated env variable
+    data['appName'] = APP_NAME
 
     return data;
-}
+};
+
+
